1ad62d2659f10760734c73606b97537762be6550
Yevhenii Sirenko <ew_sirenko@ukr.net>
2022-02-13 20:38:21 +0200

    Add icon

--- PhotoshopCoordGetter/Form1.Designer.cs
+++ PhotoshopCoordGetter/Form1.Designer.cs
@@ -547,6 +547,7 @@ namespace PhotoshopCoordGetter
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Photoshop Info Getter";
